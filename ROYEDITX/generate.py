487cc8922f0287a6f54a1c46b23af26a3e6dc304
innocentbabe <145816333+innocentbabe@users.noreply.github.com>
2024-05-31 17:44:01 +0530

    Update generate.py

--- ROYEDITX/generate.py
+++ ROYEDITX/generate.py
@@ -174,7 +174,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
         string_session = client.session.save()
     else:
         string_session = await client.export_session_string()
-    text = f"**❖ ᴛʜɪs ɪs ʏᴏᴜʀ {ty} sᴛʀɪɴɢ sᴇssɪᴏɴ** \n\n`{string_session}` \n\n**● ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➥ ** @Infinity_Backup\n● **ɴᴏᴛᴇ ➥ ** ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ʙᴇᴄᴀᴜsᴇ ʜᴇ ᴄᴀɴ ʜᴀᴄᴋ ʏᴏᴜʀ ᴀʟʟ ᴅᴀᴛᴀ. ᴀɴᴅ ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ ᴊᴏɪɴ ➥ @THE_FRIENDZ 🥺"
+    text = f"**❖ ᴛʜɪs ɪs ʏᴏᴜʀ {ty} sᴛʀɪɴɢ sᴇssɪᴏɴ** \n\n`{string_session}` \n\n**● ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➥ ** @ItachiSessionBot\n● **ɴᴏᴛᴇ ➥ ** ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ʙᴇᴄᴀᴜsᴇ ʜᴇ ᴄᴀɴ ʜᴀᴄᴋ ʏᴏᴜʀ ᴀʟʟ ᴅᴀᴛᴀ. ᴀɴᴅ ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ ᴊᴏɪɴ ➥ @Infinity_Backup 🍁"
     try:
         if not is_bot:
             await client.send_message("me", text)
@@ -183,7 +183,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
     except KeyError:
         pass
     await client.disconnect()
-    await bot.send_message(msg.chat.id, "⬤ sᴜᴄᴄᴇssғᴜʟʟʏ ɢʀɴᴇʀᴀᴛᴇᴅ ʏᴏᴜ {} sᴛʀɪɴɢ sᴇssɪᴏɴ.\n\n● ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇ ᴛᴏ ɢᴇᴛ ɪᴛ ! \n\n● ᴀ sᴛʀɪɴɢ  ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ʙʏ ➥ @RoY_EdiTX 💌".format("ᴛᴇʟᴇᴛʜᴏɴ" if telethon else "ᴩʏʀᴏɢʀᴀᴍ"))
+    await bot.send_message(msg.chat.id, "⬤ sᴜᴄᴄᴇssғᴜʟʟʏ ɢʀɴᴇʀᴀᴛᴇᴅ ʏᴏᴜ {} sᴛʀɪɴɢ sᴇssɪᴏɴ.\n\n● ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇ ᴛᴏ ɢᴇᴛ ɪᴛ ! \n\n● ᴀ sᴛʀɪɴɢ  ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ʙʏ ➥ @Infinity_Backup ❄️".format("ᴛᴇʟᴇᴛʜᴏɴ" if telethon else "ᴩʏʀᴏɢʀᴀᴍ"))
 
 
 async def cancelled(msg):
