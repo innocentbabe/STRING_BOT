52fed2a10e96bae5fae50ea4d3dcef921e2d5f5f
innocentbabe <145816333+innocentbabe@users.noreply.github.com>
2024-05-31 17:58:22 +0530

    Itachi must_join.py

--- ROYEDITX/must_join.py
+++ ROYEDITX/must_join.py
@@ -20,7 +20,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/4bd4e28e31194e1820bf5.jpg", caption=f"❖ ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ➥ [sᴜᴘᴘᴏʀᴛ]({link}) ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ,",
+                    photo="https://te.legra.ph/file/4bb8d12905dd0f1b2737e.jpg", caption=f"❖ ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ➥ [sᴜᴘᴘᴏʀᴛ]({link}) ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ,",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
